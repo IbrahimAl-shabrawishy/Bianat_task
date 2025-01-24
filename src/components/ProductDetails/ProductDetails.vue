6921fe77348796ad964726dc85824bb9ae40fab1
Eng/Ibrahim AL-shabrawishy <ibrahimelshabrawishy123321@gmail.com>
2025-01-24 20:11:33 +0200

    Completed Product Details Page implementation

--- src/components/ProductDetails/ProductDetails.vue
+++ src/components/ProductDetails/ProductDetails.vue
@@ -1,11 +1,126 @@
 <template>
-    <div v-if="loading">جاري التحميل...</div>
-    <div v-else-if="error">حدث خطأ أثناء تحميل البيانات: {{ error.message }}</div>
+    <div v-if="loading">
+
+        <Loading />
+    </div>
+    <div v-else-if="error"> {{ error.message }}</div>
     <div v-else>
-        <!-- تحقق إذا كانت البيانات موجودة قبل العرض -->
+
         <div v-if="product">
-            <h1>{{ product.title }}</h1>
-            <p>السعر: {{ product.price }}</p>
+            <div class="bg-gray-100">
+                <div class="container mx-auto px-4 py-8">
+                    <div class="flex flex-wrap -mx-4">
+                        <!-- Product Images -->
+                        <div class="w-full md:w-1/2 px-4 mb-8">
+                            <img loading="lazy" :src="product.images[0]" :alt="product.title"
+                                class=" rounded-lg shadow-md mb-4" id="mainImage">
+                            <div class="flex gap-4 py-4 justify-center overflow-x-auto">
+                                <img loading="lazy" :src="product.images[1]" :alt="product.title"
+                                    class="size-16 sm:size-20 object-cover rounded-md cursor-pointer opacity-60 hover:opacity-100 transition duration-300"
+                                    onclick="changeImage(this.src)">
+                                <img loading="lazy" :src="product.images[2]" :alt="product.title"
+                                    class="size-16 sm:size-20 object-cover rounded-md cursor-pointer opacity-60 hover:opacity-100 transition duration-300"
+                                    onclick="changeImage(this.src)">
+                                <img loading="lazy" :src="product.images[0]" :alt="product.title"
+                                    class="size-16 sm:size-20 object-cover rounded-md cursor-pointer opacity-60 hover:opacity-100 transition duration-300"
+                                    onclick="changeImage(this.src)">
+                                <img loading="lazy" :src="product.images[1]" :alt="product.title"
+                                    class="size-16 sm:size-20 object-cover rounded-md cursor-pointer opacity-60 hover:opacity-100 transition duration-300"
+                                    onclick="changeImage(this.src)">
+                            </div>
+                        </div>
+
+                        <!-- Product Details -->
+                        <div class="w-full md:w-1/2 px-4 py-11">
+                            <h2 class="text-3xl font-bold mb-2">{{ product.title }}</h2>
+                            <div class="mb-4">
+                                <span class="text-2xl font-bold mr-2">{{ product.price }}</span>
+                                <span class="text-gray-500 line-through">$100.99</span>
+                            </div>
+                            <div class="flex items-center mb-4">
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
+                                    class="size-6 text-yellow-500">
+                                    <path fill-rule="evenodd"
+                                        d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.006 5.404.434c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.434 2.082-5.005Z"
+                                        clip-rule="evenodd" />
+                                </svg>
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
+                                    class="size-6 text-yellow-500">
+                                    <path fill-rule="evenodd"
+                                        d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.006 5.404.434c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.434 2.082-5.005Z"
+                                        clip-rule="evenodd" />
+                                </svg>
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
+                                    class="size-6 text-yellow-500">
+                                    <path fill-rule="evenodd"
+                                        d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.006 5.404.434c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.434 2.082-5.005Z"
+                                        clip-rule="evenodd" />
+                                </svg>
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
+                                    class="size-6 text-yellow-500">
+                                    <path fill-rule="evenodd"
+                                        d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.006 5.404.434c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.434 2.082-5.005Z"
+                                        clip-rule="evenodd" />
+                                </svg>
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
+                                    class="size-6 text-yellow-500">
+                                    <path fill-rule="evenodd"
+                                        d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.006 5.404.434c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.434 2.082-5.005Z"
+                                        clip-rule="evenodd" />
+                                </svg>
+                                <span class="ml-2 text-gray-600">4.5 (120 reviews)</span>
+                            </div>
+                            <p class="text-gray-700 mb-6">{{ product.description }}</p>
+
+
+
+
+
+                            <div class="flex space-x-4 mb-6">
+                                <button
+                                    class="bg-indigo-600 flex gap-2 items-center text-white px-6 py-2 rounded-md hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                        stroke-width="1.5" stroke="currentColor" class="size-6">
+                                        <path stroke-linecap="round" stroke-linejoin="round"
+                                            d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z" />
+                                    </svg>
+                                    Add to Cart
+                                </button>
+
+                                <router-link to="/home">
+                                    <button
+                                        class="bg-indigo-600 flex gap-2 items-center text-white px-6 py-2 rounded-md hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
+
+                                        Home Page
+                                    </button>
+                                </router-link>
+
+
+
+
+
+
+
+
+                            </div>
+
+
+
+                        </div>
+                    </div>
+                </div>
+
+
+            </div>
+
+
+
+
+
+
+
+
+
         </div>
         <div v-else>
             <p>المنتج غير موجود.</p>
@@ -18,8 +133,11 @@ import { useRoute } from 'vue-router';
 import { useQuery } from '@vue/apollo-composable';
 import gql from 'graphql-tag';
 import { ref, watch } from 'vue';
-
+import Loading from '../../components/Loading/Loading.vue';
 export default {
+    components: {
+        Loading
+    },
     setup() {
         const route = useRoute();
         const productId = route.params.id;  // الحصول على الـ id من الرابط
@@ -32,6 +150,8 @@ export default {
           product(id: $id) {
             title
             price
+            images
+            description
             
           }
         }
@@ -62,4 +182,9 @@ export default {
 };
 </script>
 
-<style scoped></style>
+<style lang="scss" scoped>
+#mainImage {
+    width: 100%;
+    height: 500px;
+}
+</style>
